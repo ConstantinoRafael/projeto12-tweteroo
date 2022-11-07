2ac9d882753ce50b8fd412fef5fff040e74a16ec
ConstantinoRafael <rafael.constantino89@gmail.com>
2022-11-07 03:28:49 -0300

    perf: melhoria na disposição dos tweets

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ app.use(express.json());
 
 const users = [];
 
-
-
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
 
@@ -27,21 +25,19 @@ const tweets = [];
 
 app.post("/tweets", (req, res) => {
   const { username, tweet } = req.body;
-  const { avatar } = users.find((u) => (u.username === username));
+  const { avatar } = users.find((u) => u.username === username);
   const newTweet = {
     username,
     avatar,
     tweet,
   };
 
-  tweets.push(newTweet);
+  tweets.unshift(newTweet);
 
   res.send(tweets);
 });
 
 app.get("/tweets", (req, res) => {
-  
-
   res.send(tweets);
 });
 
