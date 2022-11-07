a77910630932197e6383dc553c84e604db1bae1a
ConstantinoRafael <rafael.constantino89@gmail.com>
2022-11-07 02:12:08 -0300

    Versão final com os requisitos básicos

--- index.js
+++ index.js
@@ -8,44 +8,39 @@ app.use(express.json());
 
 const users = [];
 
+
+
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
 
   const newUser = {
-    id: users.length + 1,
     username,
     avatar,
   };
 
   users.push(newUser);
 
-  res.send("OK");
+  res.send(users);
 });
 
 const tweets = [];
 
 app.post("/tweets", (req, res) => {
   const { username, tweet } = req.body;
+  const { avatar } = users.find((u) => (u.username === username));
   const newTweet = {
-    id: tweets.length + 1,
-    username,
-    tweet,
-  };
-
-  tweets.push(newTweet);
-
-  res.send("OK");
-});
-
-app.get("/tweets", (req, res) => {
-  const { username, avatar, tweet } = req.query;
-  const oldTweet = {
     username,
     avatar,
     tweet,
   };
 
-  tweets.push(oldTweet);
+  tweets.push(newTweet);
+
+  res.send(tweets);
+});
+
+app.get("/tweets", (req, res) => {
+  
 
   res.send(tweets);
 });
